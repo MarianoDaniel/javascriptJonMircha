e32c5586aacea679b92eba885af540ed949b1d13
mavico <avicomariano@gmail.com>
2021-11-18 23:37:52 -0300

    []

--- DOM Eventos-con-Parámetros-y-Remover-Eventos.js
+++ DOM Eventos-con-Parámetros-y-Remover-Eventos.js
@@ -1,2 +1,24 @@
 //DOM: Eventos con Parámetros y Remover Eventos
 
+function saludar(nombre = "Desconocido") {
+    //nombre = prompt("indique su nombre")
+    alert(`Hola ${nombre}`)
+}
+
+const $eventoMultiple = document.getElementById("evento-multiple")
+//Para pasar parámetros utilizamos como handler function una función anonima o arrow
+$eventoMultiple.addEventListener("click", () => {
+    saludar()
+    saludar("Mariano")
+})
+
+//Remover eventos
+const $eventRemove = document.getElementById("evento-remover")
+
+const removerDobleClick = (e) => {
+    alert(`Removiendo el evento de tipo ${e.type}`)
+    $eventRemove.removeEventListener("dblclick", removerDobleClick)
+    $eventRemove.disabled = true
+}
+
+$eventRemove.addEventListener("dblclick", removerDobleClick)
