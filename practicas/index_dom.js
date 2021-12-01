004dd5e4c4d8a5bea117a536d54285ff5bc3470a
mavico <avicomariano@gmail.com>
2021-11-30 23:19:36 -0300

    colisiones

--- practicas/index_dom.js
+++ practicas/index_dom.js
@@ -1,5 +1,7 @@
 import hamburguerMenu from "./menu_hamburguesa.js";
 import { relojYalarma, alarm } from "./reloj_y_alarma.js";
+//import { shortcuts } from "./teclado.js";
+import { moveBall } from "./teclado.js";
 
 //Vamos a cargarlo con el DOMContentLoaded
 const d = document
@@ -7,6 +9,9 @@ d.addEventListener("DOMContentLoaded", e => {
     //le paso las clases y el parámetro menú a para que al presionarlo se cierre el panel
     hamburguerMenu(".panel-btn", ".panel", ".menu a")
     relojYalarma("#iniciar-reloj", "#detener-reloj", "#panel-reloj p", "#panel-reloj")
-    alarm("assets/alarm.mp3","#iniciar-alarma","#detener-alarma")
+    alarm("assets/alarm.mp3", "#iniciar-alarma", "#detener-alarma")
 })
-
+d.addEventListener("keydown", e => {
+    //shortcuts(e)
+    moveBall(e, ".ball", ".stage")
+})
---
+++ practicas/teclado.js
@@ -0,0 +1,76 @@
+const d = document
+let x = 0,
+    y = 0;
+export function moveBall(e, ball, stage) {
+    const $ball = d.querySelector(ball),
+        $stage = d.querySelector(stage),
+        //Colisiones
+        limitsBall = $ball.getBoundingClientRect(),
+        limitStage = $stage.getBoundingClientRect();
+    console.log(limitsBall)
+    console.log(limitStage)
+
+    console.log(e.key)
+    console.log(e.keyCode)
+
+    //función que va a recibir la dirección y en base a eso se toman las decisiones
+
+
+    switch (e.keyCode) {
+        case 37:
+            if (limitsBall.left > limitStage.left) {
+                e.preventDefault()
+                x--
+            }
+
+            break;
+        case 38:
+            if (limitsBall.top > limitStage.top) {
+                e.preventDefault()
+                y--
+            }
+
+
+            break;
+        case 39:
+            if (limitsBall.right < limitStage.right) {
+                e.preventDefault()
+                x++
+            }
+
+            break;
+        case 40:
+            if (limitsBall.bottom < limitStage.bottom) {
+                e.preventDefault()
+                y++
+            }
+
+            break;
+
+        default:
+            break;
+
+    }
+    $ball.style.transform = `translate(${x * 10}px, ${y * 10}px)`
+
+}
+
+/* export function shortcuts(e) {
+    console.log(e.type)
+    console.log(e.key)
+    console.log(e.keyCode)
+    console.log(`ctrl: ${e.ctrlKey}`)
+    console.log(`alt: ${e.altKey}`)
+    console.log(`shift: ${e.shiftKey}`)
+    console.log(e)
+
+    if (e.key === "a" && e.altKey) {
+        alert("Haz lanzado una alerta con el Teclado")
+    }
+    if (e.key === "c" && e.altKey) {
+        confirm("Haz lanzado una confirmación con el Teclado")
+    }
+    if (e.key === "p" && e.altKey) {
+        prompt("Haz lanzado un aviso con el Teclado")
+    }
+} */
